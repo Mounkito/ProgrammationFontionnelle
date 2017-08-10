12556e9e08355a87ae8a25f9f37121b36cd6c372
Yohan Lamerand <yohan.lamerand@lcdlv.fr>
2017-08-10 10:42:48 +0200

    Refactor : remove dead code

--- src/main/java/functional/CashRegister.java
+++ src/main/java/functional/CashRegister.java
@@ -2,9 +2,6 @@ package functional;
 
 public class CashRegister {
 
-    public Price total(Price price, Quantity quantity) {
-        return price.multiplyBy(quantity);
-    }
     public Result total(Result result, Quantity quantity) {
         return result.map(price -> price.multiplyBy(quantity));
     }
